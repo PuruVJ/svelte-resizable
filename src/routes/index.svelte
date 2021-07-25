2e15a832fd4b72b5a3efd793463e4778bb29f218
Puru Vijay <devpuruvj@gmail.com>
2021-07-26 00:12:46 +0530

    Push changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,5 +6,5 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <Resizable bounds="parent" defaultSize={{ height: '400px', width: '60vw' }}>
-  <div style="height: 100%; width: 100%; background-color: #6002ee" />
+  <div style="height: 100%; width: 100%; background-color: #6002ee; border-radius: 0.5rem;" />
 </Resizable>
